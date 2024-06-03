335e607408ab53f406cab4212f12e18d4c227f5b
efrutos <52461710+efrutos@users.noreply.github.com>
2024-06-03 17:15:56 +0200

    Add files via upload

--- get_next_line.h
+++ get_next_line.h
@@ -6,12 +6,16 @@
 /*   By: estfruto <estfruto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/27 08:40:57 by estfruto          #+#    #+#             */
-/*   Updated: 2023/11/02 16:44:32 by estfruto         ###   ########.fr       */
+/*   Updated: 2023/11/10 10:13:58 by estfruto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+# endif
 
 # include <stdlib.h>
 # include <unistd.h> 
@@ -22,11 +26,11 @@ char	*get_next_line(int fd);
 char	*read_buffer(int fd, char *temp);
 char	*full_line(char	*temp);
 char	*rest_line(char *temp);
+char	*free_temp(char	**str);
 void	*ft_calloc(size_t count, size_t size);
 size_t	ft_strlen(const char	*str);
-char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strjoin_mod(char *s1, char *s2);
 char	*ft_strchr(const char *s, int c);
-void	*ft_memmove(void *dst, const void *src, size_t n);
 char	*ft_strdup(char *input);
 
 #endif
